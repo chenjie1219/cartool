9fad828c0228dd94dd90ca74a8143aa1a6f3d273
chenjie <4234115@qq.com>
2019-01-08 13:41:07 +0800

    Initial Commit

---
+++ cartool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  cartool
+//
+//  Created by Jason on 2019/1/8.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ cartool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cartool
+//
+//  Created by Jason on 2019/1/8.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
